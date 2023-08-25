e9740ced00e4268f7de5063733b83b27f9d3248a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-25 16:24:32 -0300

    feat: LazyEager
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/dev/matheusvictor/gof/SingletonEager.java
@@ -0,0 +1,20 @@
+package dev.matheusvictor.gof;
+
+
+/*
+ *
+ * SingletonEager
+ * @author MatheusVict
+ * */
+public class SingletonEager {
+
+  private static SingletonEager instance = new SingletonEager();
+
+  private SingletonEager() {
+    super();
+  }
+
+  public static SingletonEager getInstance() {
+    return instance;
+  }
+}
