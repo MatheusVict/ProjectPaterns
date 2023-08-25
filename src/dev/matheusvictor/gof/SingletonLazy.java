05725b7c73c2a3d61f5deb9dc30091946116fdc2
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-25 16:20:24 -0300

    feat: SingletonLazy
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/dev/matheusvictor/gof/SingletonLazy.java
@@ -0,0 +1,19 @@
+package dev.matheusvictor.gof;
+
+/*
+* SingletonLazy
+* @author MatheusVict
+* */
+public class SingletonLazy {
+  private static SingletonLazy instance;
+
+  private SingletonLazy() {
+    super();
+  }
+
+  public static SingletonLazy getInstance() {
+    if (instance == null)
+      instance = new SingletonLazy();
+    return instance;
+  }
+}
